0c09381b5155746f9623fc38c0bb60bb49b68e05
Shriyansh Singh <shriyansh.singh01@student.csulb.edu>
2024-02-26 23:44:10 -0800

    Adding a Vigenere cipher

--- Exercise1.py
+++ Exercise1.py
@@ -1,6 +1,6 @@
 '''
 Writing a Caesar Cipher
-- Shifts each letter with the same offset
+- Shifts each letter with the same given offset
 '''
 def caesar(message, offset):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
@@ -13,5 +13,29 @@ def caesar(message, offset):
             index = alphabet.find(char)
             new_index = (index + offset) % len(alphabet)
             encrypted_text += alphabet[new_index]
-    
     return 'plain text:', message + '\nencrypted text:', encrypted_text
+
+'''
+Writing a Vigenere Cipher
+- Uses a keyword to shift each letter
+'''
+def vigenere(message, key, direction):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    final_message = ''
+
+    for char in message.lower():
+        # Append space to the message
+        if char == ' ':
+            final_message += char
+        else:
+            # Find the right key character to encode
+            key_char = key[key_index % len(key)]
+            key_index += 1
+
+            # Define the offset and the encrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    return final_message
