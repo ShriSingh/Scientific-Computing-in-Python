a9236944ff73bbc1f9909843d44b5e4c958a7e0a
Shriyansh Singh <shriyansh.singh01@student.csulb.edu>
2024-02-26 23:41:16 -0800

    Making changes to the Caesar cipher

--- Exercise1.py
+++ Exercise1.py
@@ -13,5 +13,5 @@ def caesar(message, offset):
             index = alphabet.find(char)
             new_index = (index + offset) % len(alphabet)
             encrypted_text += alphabet[new_index]
-    print('plain text:', message)
-    print('encrypted text:', encrypted_text)
+    
+    return 'plain text:', message + '\nencrypted text:', encrypted_text
