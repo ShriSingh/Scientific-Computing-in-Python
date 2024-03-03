0588cce90423f07935be75fd7ea868e3584032f6
Shriyansh Singh <shriyansh.singh01@student.csulb.edu>
2024-03-02 19:56:47 -0800

    Editing the way the amount of an expense is shown

--- Exercise3.py
+++ Exercise3.py
@@ -1,18 +1,25 @@
 '''
 Building a Expense Tracker
+    - Learnt using lambda functions in Python
+        - Lambda Functions are brief, anonymous functions in Python
+            - Ideal for simple and one-time tasks
+        - Syntax: lambda x: expr, where 'x' -> parameter and 'expr' -> expression
+        - Example: lambda x: x ** 2 -> returns the square of the input
 '''
-
 def add_expense(expenses, amount, category):
+    # Append a dictionary to the list of expenses
     expenses.append({'amount': amount, 'category': category})
 
 def print_expenses(expenses):
     for expense in expenses:
-        print(f'Amount: {expense["amount"]}, Category: {expense["category"]}')
+        print(f'Amount: ${expense["amount"]}, Category: {expense["category"]}')
 
 def total_expenses(expenses):
+    # Using a lambda function add up all the amounts in the list of expenses
     return sum(map(lambda expense: expense['amount'], expenses))
 
 def filter_expenses_by_category(expenses, category):
+    # Using a lambda function to identify and filter the expenses by category
     return filter(lambda expense: expense['category'] == category, expenses)
 
 def main():
@@ -29,14 +36,14 @@ def main():
         choice = input('Enter your choice: ')
 
         if choice == '1':
-            amount = float(input('Enter amount: '))
+            amount = float(input('Enter amount: $'))
             category = input('Enter category: ')
             add_expense(expenses, amount, category)
         elif choice == '2':
             print('\nAll Expenses:')
             print_expenses(expenses)
         elif choice == '3':
-            print('\nTotal Expenses: ', total_expenses(expenses))
+            print('\nTotal Expenses: $', total_expenses(expenses))
         elif choice == '4':
             category = input('Enter category to filter: ')
             print(f'\nExpenses for {category}:')
