8b2e410b65e8b1aafbfdc7a550c6fdd2abb4ad6c
Shriyansh Singh <shriyansh.singh01@student.csulb.edu>
2024-03-03 12:12:23 -0800

    Completing 'Learning List Comprehension by Building a Case Converter Program'

--- Exercise4.py
+++ Exercise4.py
@@ -21,9 +21,19 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
 
     return clean_snake_cased_string
 
+def convert_to_snake_case_2(pascal_or_camel_cased_string):
+    snake_cased_char_list = [
+        '_' + char.lower() if char.isupper()        # --> place '_' & concatenate a lowercase of the letter if it's in uppercase
+        else char                                   # --> Otherwise put a the default letter
+        for char in pascal_or_camel_cased_string    # --> For every character in the string
+    ]
+
+    return ''.join(snake_cased_char_list).strip('_')    # Doing a combined operation of joining the list and stripping any dangling underscores
+
 def main():
     input_string = input('Enter a string in Camel Case or Pascal Case: ')
-    print(f'Snake Case: {convert_to_snake_case(input_string)}')
+    print(f'Snake Case using normal version: {convert_to_snake_case(input_string)}')
+    print(f'Snake Case using list comprehension: {convert_to_snake_case_2(input_string)}')
 
 if __name__ == '__main__':
     main()
