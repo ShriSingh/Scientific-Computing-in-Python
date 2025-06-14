033d176f1edaa8d51646af2ef93d30a5a9e8d01b
ShriSingh <shrisingh9416@gmail.com>
2025-06-14 00:12:39 -0700

    Setting up parameters

--- Projects/Polygon Area Calculator/polygon_area_calculator.py
+++ Projects/Polygon Area Calculator/polygon_area_calculator.py
@@ -1,1 +1,53 @@
-# Building a Polygon Area Calculto
+# Building a Polygon Area Calculator
+
+class Rectangle:
+    """
+    Should initialize a 'rectangle' object with
+    'width' and 'height' attributes. Needs to be 
+    represented by 'Rectangle(width=5, height=10)'
+    It should also contain:
+        - set_width
+        - set_height
+        - get_area: returns area (width * height)
+        - get_perimeter: (2 * width + 2 * height)
+        - get_diagonal: (width ** 2 + height ** 2) ** .5
+        - get_picture: Returns a string that represents the shape
+        - get_amount_inside: Returns the number of times the passed in shape
+        could fit inside the shape(with no rotations)
+    """
+
+    def __init__(self, width, height):
+        """
+        Creating a Rectangle class with a constructor 
+        that takes in width and height to calculate its 
+        various attributes.
+
+        Parameters:
+            width : float
+                The width of the rectangle
+            height : float
+                The height of the rectangle
+        
+        """
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        """
+        Sets the width of the rectangle.
+
+        Parameters:
+            width : float
+                The new width of the rectangle
+        """
+        self.width = width
+
+    def set_height(self, height):
+        """
+        Sets the height of the rectangle
+
+        Parameters:
+            height : float
+                The new height of the rectangle
+        """
+        self.height = height
