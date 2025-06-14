98f0036b98713614c56b4f9ddf3249cbf8d0cf32
ShriSingh <shrisingh9416@gmail.com>
2025-06-14 00:27:29 -0700

    Calculating area, perimeter, and diagonal of the rectangle

--- Projects/Polygon Area Calculator/polygon_area_calculator.py
+++ Projects/Polygon Area Calculator/polygon_area_calculator.py
@@ -5,15 +5,16 @@ class Rectangle:
     Should initialize a 'rectangle' object with
     'width' and 'height' attributes. Needs to be 
     represented by 'Rectangle(width=5, height=10)'
+    
     It should also contain:
-        - set_width
-        - set_height
-        - get_area: returns area (width * height)
-        - get_perimeter: (2 * width + 2 * height)
-        - get_diagonal: (width ** 2 + height ** 2) ** .5
-        - get_picture: Returns a string that represents the shape
-        - get_amount_inside: Returns the number of times the passed in shape
-        could fit inside the shape(with no rotations)
+        set_width
+        set_height
+        get_area: returns area (width * height)
+        get_perimeter: (2 * width + 2 * height)
+        get_diagonal: (width ** 2 + height ** 2) ** .5
+        get_picture: Returns a string that represents the shape
+        get_amount_inside: Returns the number of times the passed in shape
+            could fit inside the shape(with no rotations)
     """
 
     def __init__(self, width, height):
@@ -23,9 +24,11 @@ class Rectangle:
         various attributes.
 
         Parameters:
-            width : float
+            self (object):
+                The rectangle object
+            width (float): 
                 The width of the rectangle
-            height : float
+            height (float): 
                 The height of the rectangle
         
         """
@@ -34,10 +37,12 @@ class Rectangle:
 
     def set_width(self, width):
         """
-        Sets the width of the rectangle.
+        Sets the width of the rectangle
 
         Parameters:
-            width : float
+            self (object):
+                The rectangle object
+            width (float): 
                 The new width of the rectangle
         """
         self.width = width
@@ -47,7 +52,57 @@ class Rectangle:
         Sets the height of the rectangle
 
         Parameters:
-            height : float
+            self (object): 
+                The rectangle object
+            height (float): 
                 The new height of the rectangle
         """
         self.height = height
+
+    def get_area(self):
+        """
+        Calculates the area of a rectangle
+
+        Parameters:
+            self (object):
+                The rectangle object
+
+        Returns:
+            area (float):
+                The area of the rectangle
+        """
+        area = self.width * self.height
+
+        return area
+
+    def get_perimeter(self):
+        """
+        Calculates the perimeter, or boundary length, of the rectangle
+
+        Paramters:
+            self (object):
+                The rectangle object
+
+        Returns:
+            perimeter (float):
+                The perimeter(or boundary length) of the rectangle
+        """
+        perimeter = (2 * self.width) + (2 * self.height)
+
+        return perimeter
+    
+    def get_diagonal(self):
+        """
+        Calculates the length of the diagonal of the rectangle
+
+        Parameters:
+            self (object):
+                The rectangle object
+
+        Returns:
+            diagonal_length (float):
+                The length of the diagonal of the rectangle
+        """
+        diagonal_length = (((self.width ** 2) + (self.height ** 2)) ** 0.5)
+
+        return diagonal_length
