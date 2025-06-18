055ccb9ad4f60ae3597d4eebb289e86328f07827
ShriSingh <shrisingh9416@gmail.com>
2025-06-18 00:39:08 -0700

    Creating the file for creating a probability calculator. Creating a Hat class and functions

---
+++ Projects/Probability Calculator/probability_calculator.py
@@ -0,0 +1,38 @@
+"""
+Building a Probability Calculator project
+- Determine the approximate probability of drawing certain
+balls randomly from a hat
+"""
+
+import copy
+import random
+
+class Hat:
+    """
+    Takes a variable number of arguments that specify the number
+    of balls of each color that are in that hat
+    """
+    pass
+
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    """
+    Calculates the probability of certain balls drawn from a hat
+
+    Parameters:
+        hat (object):
+            Object containing balls
+        expected_balls (object):
+            Indicates the exact group of balls will be drawn
+        num_balls_drawn (int):
+            Number of balls to draw out of the hat
+        num_experiments (int):
+            Number of experiments to perform. The more experiments
+            are performed, the more accurate the approximate
+            probability will be
+    Returns:
+        probability (int):
+            Probability values for a certain expected balls drawn
+            from a hat
+    """
+    pass
