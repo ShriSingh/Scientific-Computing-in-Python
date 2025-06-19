b9447f671a3eb41b16eced3efb22e980094575cf
ShriSingh <shrisingh9416@gmail.com>
2025-06-18 17:17:04 -0700

    Editing init()  and adding draw() for Hat class, testing Hat object creation

--- Projects/Probability Calculator/probability_calculator.py
+++ Projects/Probability Calculator/probability_calculator.py
@@ -24,16 +24,44 @@ class Hat:
             **kwargs:
                 Multiple ball colors of various quantities
         """
-        # A list to store the collection of balls in the "hat"
-        self.collection = []
+        # A list to store the collection of ball colors in the "hat"
+        self.contents = []
         # Going thru every ball of every color
         for color, amount in kwargs.items():
-            # Setting the attributes(color and quantity) of ball
+            # Setting the attributes(color and quantity) of each ball
             setattr(self, color, amount)
             # Adding all the balls to the list
             for _ in range(0, amount):
-                self.collection.append(color)
+                self.contents.append(color)
 
+    def draw(self, balls_drawn):
+        """
+        Method that indicates the number of balls drawn
+        from the hat. Removes the balls at random and be returned
+        as a list of strings(shouldn't go back into the hat).
+        Returns all the balls if the number of balls to draw 
+        exceeds the available quantity.
+
+        Parameters:
+            self (object):
+                The instance of the Hat object
+            balls_drawn (int):
+                Number of balls drawn from the hat
+        """
+        # Storing the list of balls drawn from the hat
+        draw_list = []
+        # Storing the balls list in the hat
+        contents = self.contents
+        # Comparing the number of balls drawn from the hat to the number of balls in the hat
+        if balls_drawn > len(contents):
+            return contents
+        else:
+            while len(draw_list) < balls_drawn:
+                index = random.randint(0, len(contents) - 1)
+                ball = contents[index]
+                contents.remove(ball)
+                draw_list.append(contents)
+        return draw_list
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     """
@@ -56,3 +84,7 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
             from a hat
     """
     pass
+
+if __name__ == "__main__":
+    hat = Hat(red=1, blue=3, green=2)
+    print(hat)
